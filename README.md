aa7409cb241d3752d9fcebaf9e86a7c072d73e83
iltempe <teosoft80@gmail.com>
2020-04-11 18:28:06 +0200

    updated with ALL reports

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ At the moment active here https://covid19italiahelp.herokuapp.com/
 To use that  
 
 ## GET
-reports/data_field where data_field is one field name of the data list present in the issue dataset here
+reports/ get ALL reports in JSON format
+reports/data_field where "data_field" is one tag name of the reports list present in the issue dataset here
 https://raw.githubusercontent.com/emergenzeHack/covid19italia_data/master/issuesjson.json
-
 Example
 https://host/reports/Provincia
 gives all the "Provincia" values of all reports (where exist).
 
-To start python3 app.py
+To start locally python3 app.py
--- app.py
+++ app.py
@@ -20,12 +20,30 @@ def reports(data_field):
     print("Started reading JSON data...")
     # We can then find the data for the requested and send it back as json
     newlist = []
+    newlist1 = []
     while i < len(file_data):
-        if data_field in file_data[i]['issue']['data']:
-            #print(file_data[i]['issue']['data'][data_field])
-            newlist.append(file_data[i]['issue']['data'][data_field])
+        if data_field in file_data[i]['issue']['data'] and "Descrizione" in file_data[i]['issue']['data']:
+            newlist.append(file_data[i]['issue']['data']["Descrizione"]) 
+            newlist1.append(file_data[i]['issue']['data'][data_field])
+        i += 1
+    data_set = [{'Description': country, str(data_field): wins} for country, wins in zip(newlist, newlist1)]
+    return json.dumps(data_set)
+    
+#all reports
+@app.route('/reports/', methods=['GET'])
+def reports_all():
+    i=0
+    data = urllib.request.urlopen("https://raw.githubusercontent.com/emergenzeHack/covid19italia_data/master/issuesjson.json").read()
+    file_data = json.loads(data)
+    print("Started reading JSON data...")
+    # We can then find the data for the requested and send it back as json
+    newlist = []
+    while i < len(file_data):
+        #print(file_data[i]['issue']['data'][data_field])
+        newlist.append(file_data[i]['issue']['data'])
         i += 1
     return json.dumps(newlist)
+
 
 # A welcome message to test our server
 @app.route('/')
