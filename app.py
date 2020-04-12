9a792a0e47dfdd82fcc6d06817e9a14929867102
iltempe <teosoft80@gmail.com>
2020-04-12 22:27:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ app = Flask(__name__)
 
 # establish a Flask route so that we can serve HTTP traffic on that route
 # GET api is endpoint/reports/tag_of_data_array
-# return JSON with data with all value of "data_field" (example "Titolo") where data_field exist
+# return JSON with data with all value of "data_field" (example "Provincia") where data_field exist
 @app.route('/reports/<data_field>', methods=['GET'])
-def reports(data_field):
+def reports_key(data_field):
     data = urllib.request.urlopen("https://raw.githubusercontent.com/emergenzeHack/covid19italia_data/master/issuesjson.json").read()
     file_data = json.loads(data)
     print("Started reading JSON data...")
@@ -25,7 +25,26 @@ def reports(data_field):
         i += 1
     return json.dumps(newlist)
 
-#get all reports
+# establish a Flask route so that we can serve HTTP traffic on that route
+# GET api is endpoint/reports/data_field/data_value
+# return JSON with data with all value of "data_field" (example "Provincia") where data_field exist 
+# and take all reports with data_field=data_value
+@app.route('/reports/<data_field>/<data_value>', methods=['GET'])
+def reports_key_value(data_field,data_value):
+    data = urllib.request.urlopen("https://raw.githubusercontent.com/emergenzeHack/covid19italia_data/master/issuesjson.json").read()
+    file_data = json.loads(data)
+    print("Started reading JSON data...")
+    # We can then find the data for the requested and send it back as json
+    i=0
+    newlist=[]
+    while i < len(file_data):
+        if data_field in file_data[i]['issue']['data']:
+            if file_data[i]['issue']['data'][data_field]== data_value:
+                newlist.append(file_data[i]['issue']['data'])
+        i += 1
+    return json.dumps(newlist)
+
+#GET ALL reports
 @app.route('/reports/', methods=['GET'])
 def reports_all():
     data = urllib.request.urlopen("https://raw.githubusercontent.com/emergenzeHack/covid19italia_data/master/issuesjson.json").read()
@@ -43,6 +62,11 @@ def reports_all():
 @app.route('/')
 def index():
     return "<h1>Welcome to the Covid19Italia.Help API ! </h1>"
+
+#error handler
+@app.errorhandler(404) 
+def invalid_route(e): 
+    return "Invalid route."
 
 # Get setup so that if we call the app directly (and it isn't being imported elsewhere)
 # it will then run the app with the debug mode as True
